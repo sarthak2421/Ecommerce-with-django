29b1400bed7d91b2e048f5f38d7d91ec2fc448fa
Sarthak <sarthakkalyani66@gmail.com>
2024-05-16 12:18:54 +0530

    All commited

--- AwesomeCart/AwesomeCart/urls.py
+++ AwesomeCart/AwesomeCart/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings             # FROM SETTINGS.PY FOR MEDIA STORING
 from django.conf.urls.static import static   # FROM SETTINGS.PY FOR MEDIA STORING
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
+    path('', views.index, name='index')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    # CHANGING MEDIA URL
